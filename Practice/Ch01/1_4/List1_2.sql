267a205b678bddffe074df872d17add74e519406
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-08-05 17:54:25 +0900

    create items table

---
+++ Practice/Ch01/1_4/List1_2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE items
+(id CHAR(4) NOT NULL,
+ name       VARCHAR(100) NOT NULL,
+ grouping   VARCHAR(32) NOT NULL,
+ unit_price INTEGER,
+ cost_price INTEGER,
+ created_at DATE,
+ PRIMARY KEY (id)
+);
